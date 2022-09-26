b281a2f50dfa349b171d19af7bf396722b324a74
Matheus Cabral <matheuscabraloli@gmail.com>
2022-09-26 18:51:43 -0300

    conexão com o wpp

--- src/class/Whatsapp.js
+++ src/class/Whatsapp.js
@@ -1,14 +1,19 @@
-class Whatsapp {
-  constructor() {
-    const { Client, LocalAuth, List, Buttons } = require('whatsapp-web.js');
-  }
+const { Client, LocalAuth, List, Buttons } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-  connectWpp() {
+module.exports = class Whatsapp {
+  constructor() {
     const client = new Client({
       authStrategy: new LocalAuth(),
       puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
       },
     });
+
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, { small: true });
+      });
+
+    return client;
   }
 }
